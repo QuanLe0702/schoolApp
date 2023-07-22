85c28866ba177f251eaea786e37e0cf26f388aa1
duiqt <mrfuc@yahoo.com>
2023-07-23 00:52:15 +0700

    updatee

--- lib/screens/Report_Card_screen/report_card_screen.dart
+++ lib/screens/Report_Card_screen/report_card_screen.dart
@@ -27,6 +27,7 @@ class _ReportCardScreenState extends State<ReportCardScreen> {
     fetchData();
   }
 
+  //fetch student data through api
   Future<void> fetchData() async {
     FlutterSecureStorage storage = const FlutterSecureStorage();
     token = await storage.read(key: 'token');
